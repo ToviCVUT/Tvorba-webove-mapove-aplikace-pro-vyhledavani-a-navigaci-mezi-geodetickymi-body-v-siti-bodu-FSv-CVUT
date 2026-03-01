053bec4e20b0e45fa019a21c82fa26e57c8be880
ToviCVUT <vincetom@cvut.cz>
2026-03-01 04:17:12 +0100

    Info panel, výběr bodů, ikony bodů, visuální úpravy

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 // MAP //
 let positionMarker = null;
 let userLat = null
@@ -5,14 +6,32 @@ let userLng = null
 let lastLatLng = null;
 let centerDistance = null;
 
-let allPoints = [];
+let allPoints = {};
+let layerID = {}
 let straightLine = null;
 let selectedPointNav = null;
 let selectedPointNavID = null;
+let straightNavActive = false
+
+const currentZone = document.getElementById("currentZone")
 const straightDistanceResult = document.getElementById("straightDistanceResult");
+const rasterDistanceResult = document.getElementById("rasterDistanceResult");
+const gpsAccuracy = document.getElementById("gpsAccuracy");
+infBtn = document.getElementById("infBtn")
+infPanel = document.getElementById("infPanel")
+
 
 let zones = null
 let activeZone = null;
+
+let selectedPoints = new Set()
+let selectedPointsSize = null
+let onlySelected = false
+const selectedPointsBtn = document.getElementById("selectedPointsBtn")
+
+
+
+//----------------------------------------------------------------------------------------------------------------------------------------------------
 
 // inicializace mapy
 const map = L.map("map", { maxZoom: 21}).setView([50.1040097, 14.3890886], 16); 
@@ -24,6 +43,7 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '&copy; OpenStreetMap contributors'
 }).addTo(map) 
 
+//----------------------------------------------------------------------------------------------------------------------------------------------------
 
 // UI //
 //grafické měřítko
@@ -33,9 +53,21 @@ L.control.scale({
   imperial: false,
 }).addTo(map); // měřítko
 
+//-----------------------------------------------------------------------------------------------------------------------------------------------------
 
-// funkce AKTUALIZACE PŘÍMÉ SPOJNICE
-const updateStraightLine = (lastLatLng, selectedPointNav) => {
+// Data
+// OBLASTI pomocí knihovny Turf
+    fetch("Data/zones/Zones_WGS84.geojson")
+      .then(res => res.json())
+      .then(data => {
+        zones = data
+        console.log(zones)
+        L.geoJSON(zones).addTo(map)});
+
+//----------------------------------------------------------------------------------------------------------------------------------------------------
+
+// FUNKCE AKTUALIZACE POLOHY UŽIVATELE
+const updateUserPosition = (lastLatLng, selectedPointNav) => {
   if (!lastLatLng || !selectedPointNav) return
 
   const lastPosition = L.latLng(lastLatLng);
@@ -43,9 +75,6 @@ const updateStraightLine = (lastLatLng, selectedPointNav) => {
   console.log(straightDistance)
 
    let straightDistanceRound = straightDistance.toFixed(0).replace(".", ",")
-
-  straightDistanceResult.innerHTML = "";
-  straightDistanceResult.innerHTML = `<button id="straightDistanceResultEndBtn">X</button><p class="straightDistanceResultText"><em>SD ${selectedPointNavID}</em>: <b>${straightDistanceRound} m</b></p>`
   
   if (!straightLine) {
     straightLine = L.polyline ([selectedPointNav, lastPosition]).addTo(map)
@@ -53,21 +82,29 @@ const updateStraightLine = (lastLatLng, selectedPointNav) => {
     straightLine.setLatLngs([selectedPointNav, lastPosition])
   };
 
-  const straightDistanceResultEndBtn = document.getElementById("straightDistanceResultEndBtn")
-  straightDistanceResultEndBtn.addEventListener("click", ()=> {
+
     straightDistanceResult.innerHTML = "";
-    straightLine.remove();
-    straightLine = null
+    straightDistanceResult.innerHTML = `<p class="straightDistanceResultText">Přímá vzdálenost <em>${selectedPointNavID}</em>: <b>${straightDistanceRound} m</b></p><button class="straightDistanceResultEndBtn" id="straightDistanceResultEndBtn">X</button>`
+
+    const straightDistanceResultEndBtn = document.getElementById("straightDistanceResultEndBtn")
+    straightDistanceResultEndBtn.addEventListener("click", ()=> {
+      straightDistanceResult.textContent = `Přímá vzdálenost (bod): --`;
+      straightLine.remove();
+      straightLine = null
+      straightNavActive = false
+      console.log(straightNavActive)
   })
 };
 
+//-------------------------------------------------------------------------------------------------------------------------------------------------------
 
-// funkce jestli je BOD V OBLASTI
+// FUNKCE - BOD V OBLASTI
     const georeference = () => {
       if (!zones || userLng == null || userLat == null) return;
 
       const lastPosition = turf.point([userLng, userLat]);
 
+      activeZone = null; //reset zóny
 
       zones.features.forEach(zone => {
         if (turf.booleanPointInPolygon(lastPosition, zone)){
@@ -79,18 +116,57 @@ const updateStraightLine = (lastLatLng, selectedPointNav) => {
 
       if (activeZone) {
         panoramaBtn.disabled = false;
-        console.log(activeZone.properties.location);
+        console.log(activeZone.properties.Location);
       } else {
         panoramaBtn.disabled = true;
         console.log("Mimo oblasti.");
       }
 
-      const currentZone = document.getElementById("currentZone");
       if (activeZone) {
-      currentZone.textContent = `current zone: ${activeZone.properties.location}`}
-      else {currentZone.textContent = `current zone: Out of any zones`}
+      currentZone.innerHTML = `<p>Aktuální zóna: <strong>${activeZone.properties.Location}</strong></p>`}
+      else {currentZone.innerHTML = `<p>Aktuální zóna: <strong>mimo oblast</strong></p>`}
       
     }
+
+//------------------------------------------------------------------------------------------------------------------------------------------------
+// FUNKCE PRO AKTULIZACI VYKRESLENÝCH BODŮ
+const renderOnlySelectedPoints = () => {
+  for(const id in layerID){
+    const layer = layerID[id]
+
+    if(onlySelected){
+      if (selectedPoints.has(Number(id))){
+        layer.addTo(map)
+       /*layer.setStyle({
+          radius: 6,
+          fillColor: "rgb(38, 242, 11)",
+          color:"#fff"})*/
+
+      
+    } else {layer.remove()}
+  } else {
+    layer.addTo(map)
+    /*layer.setStyle({radius: 6,
+          fillColor: "#0d4a87",   
+          color: "#ffffff",
+    })*/
+  }
+  };
+};
+
+// FUNKCE PRO PŘIDÁNÍ DO SEZNAMU VYBRANÝCH BODŮ PO ZAŠKRTNUTÍ V POPUPU A NÁSLEDNÉ VYKRESLENÍ POMOCÍ FUNKCE
+const pointSelect = (ID, isChecked) => {
+  if (isChecked == true){
+    selectedPoints.add(ID)
+  } else {
+    selectedPoints.delete(ID)
+  };
+
+  renderOnlySelectedPoints(); // aktualizování vykreslených bodů
+  selectedPointsBtn.textContent = selectedPoints.size
+};
+
+//-----------------------------------------------------------------------------------------------------------------------------------------------------
 
 // SLEDOVÁNÍ POLOHY UŽIVATELE
 const positionBtn = document.getElementById("gpsBtn");
@@ -100,6 +176,7 @@ navigator.geolocation.watchPosition(position => {
 
   userLat = position.coords.latitude;
   userLng = position.coords.longitude;
+  positionAccuracy = position.coords.accuracy //přesnost
 
   // poslední poloha
   lastLatLng = [userLat, userLng];
@@ -111,16 +188,26 @@ navigator.geolocation.watchPosition(position => {
     positionMarker.setLatLng(lastLatLng)
   };
 
-  updateStraightLine(lastLatLng, selectedPointNav);
+  // infPanel - kontrola oblasti
   georeference();
+
+  // infPanel - GPS přesnost
+  gpsAccuracy.innerHTML = `<p>GPS přesnost: <strong>± ${positionAccuracy} m</strong></p>`
+  
+  // infPanel - přímá vzdálenost
+  if(straightNavActive){
+  updateUserPosition(lastLatLng, selectedPointNav)};
+
 });
 
+//-------------------------------------------------------------------------------------------------------------------------------------------------------
 
-
-// GPS button
+// GPS BUTTON
 positionBtn.addEventListener("click", () => {
   map.setView(lastLatLng)
 });
+
+//------------------------------------------------------------------------------------------------------------------------------------------------------
 
  // odchylka centra mapy od polohy uživatele
   map.on("moveend", () => {
@@ -149,22 +236,36 @@ positionBtn.addEventListener("click", () => {
     console.log(map.getZoom())
   })*/
 
+//-----------------------------------------------------------------------------------------------------------------------------------------------------
 
 // DATA //
-
-
 fetch("Data/points/Points_WGS84.geojson")
   .then(res => res.json())
   .then(data => {
     allPoints = data.features
     console.log(allPoints)
-    L.geoJSON(data, {
+    const Points = L.geoJSON(data, {
+
+      pointToLayer: function (feature, latlng) {
+        return L.circleMarker(latlng, {
+          radius: 6,
+          fillColor: "#0d4a87",   // modrá výplň
+          color: "#ffffff",       // tmavší okraj
+          weight: 1,              // tloušťka okraje
+          opacity: 1,
+          fillOpacity: 0.9 
+        });
+      },
+
       onEachFeature: (feature, layer) => {
 
         // popUp bodu
         const p = feature.properties;
-        const Y = p.Y * -1
-        const X = p.X * -1
+        const Y = p.Y * -1;
+        const X = p.X * -1;
+        layerID[p.ID] = layer;
+      
+
 
         layer.bindPopup(`
          Číslo bodu: ${p.ID}<br>
@@ -172,25 +273,51 @@ fetch("Data/points/Points_WGS84.geojson")
          Y: <em>${Y}</em> m<br>
          Z: <em>${p.Z}</em> m<br>
          Typ: ${p.typ} <br>
-        <button class="straightNavigationBtn" type="button">Přímá vzdálenost</button>
-          `
+        <button class="straightNavigationBtn" type="button">Přímá vzdálenost</button><br>
+        Vybrat bod: <input type="checkbox" class="selectChecked" onchange="pointSelect(${p.ID}, this.checked)"></input>
+          `, { autoPan: false }
         )
 
         layer.on("popupopen", (e) => {
           const layerPopup = e.popup.getElement();
+
+          // checkbox pro výběr bodu zůstane v Popup zaškrtnutý
+          const checkBox = layerPopup.querySelector(".selectChecked"); 
+          checkBox.checked = selectedPoints.has(Number(p.ID));
+
+          checkBox.addEventListener("change", ()=> {
+          if(checkBox.checked){
+            layer.setStyle({
+              radius: 8,
+          fillColor: "rgb(38, 242, 11)",
+          color:"#fff"
+            })
+          } else {
+            layer.setStyle({
+            radius: 6,
+          fillColor: "#0d4a87",   
+          color: "#ffffff"
+            })
+          }})
+  
           const straightNavBtn = layerPopup.querySelector(".straightNavigationBtn");
 
           straightNavBtn.addEventListener("click", () => {
             selectedPointNav = e.target.getLatLng();
             selectedPointNavID = p.ID
-            updateStraightLine(lastLatLng, selectedPointNav)
-            console.log("klik")
+            straightNavActive = true
+            updateUserPosition(lastLatLng, selectedPointNav)
+            console.log(straightNavActive)
           }, {once: true})
+          console.log(selectedPoints)
         })
       } 
-    }).addTo(map)});
+    }) 
+    Points.addTo(map)
+  });
     
-    
+//---------------------------------------------------------------------------------------------------------------------------------------------------    
+
     // SEARCHING INPUT
     const searchingPanel = document.getElementById("searchingPanel")
     const searchingInput = document.getElementById("searchingInput")
@@ -217,10 +344,14 @@ fetch("Data/points/Points_WGS84.geojson")
        const filteredPoint = document.createElement("li")
         filteredPoint.textContent = point.properties.ID
         
+        
         filteredPoint.addEventListener("click", () => {
           const [filteredPointLat, filteredPointLng] = point.geometry.coordinates;
-        map.setView([filteredPointLng, filteredPointLat])
-        searchingPointsList.innerHTML = ""}
+        map.setView([filteredPointLng, filteredPointLat], 20)
+        searchingPointsList.innerHTML = ""
+        layerID[point.properties.ID].openPopup();
+        
+      }
         )
 
         searchingPointsList.appendChild(filteredPoint)
@@ -228,13 +359,23 @@ fetch("Data/points/Points_WGS84.geojson")
     })
 
 
-    // OBLASTI pomocí knihovny Turf
-    fetch("Data/zones/Zones_WGS84.geojson")
-      .then(res => res.json())
-      .then(data => {
-        zones = data
-        console.log(zones)
-        L.geoJSON(zones).addTo(map)});
+    // INFO BUTTON
+    infBtn = document.getElementById("infBtn")
+    infPanel = document.getElementById("infPanel")
+
+    infBtn.addEventListener("click", ()=> {
+      infPanel.classList.toggle("active");
+  })
+
+
+  // SELECTED POINTS BTN
+  selectedPointsBtn.addEventListener("click", () =>{
+    onlySelected = !onlySelected;
+    renderOnlySelectedPoints();
+    selectedPointsBtn.classList.toggle("active");
+  });
+  selectedPointsBtn.textContent = "0";
+        
 
     
     
